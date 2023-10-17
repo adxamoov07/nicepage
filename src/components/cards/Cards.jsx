4b261f325380e2e87f9bc145b257e1ef25c6a558
abdulloh <adxamovabdulloh06@gmail.com>
2023-10-17 15:10:26 +0500

    asdfgf

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
 import './Cards.css'
+import img1 from '../../assents/img1.jpg'
+import img2 from '../../assents/img2.jpg'
+import img3 from '../../assents/img3.jpg'
+import img4 from '../../assents/img4.jpg'
+import img5 from '../../assents/img5.jpg'
+import img6 from '../../assents/img6.jpg'
+
 
 function Cards() {
   return (
     <div className='cards'>
-
+      <div className="cards_item">
+        <img src={img1} alt="" />
+        <h3>GALLERY</h3>
+      </div>
+      <div className="cards_item">
+        <img src={img2} alt="img" />
+        <h3>IDEAS</h3>
+      </div>
+      <div className="cards_item">
+        <img src={img3} alt="img" />
+        <h3>AWARDS</h3>
+      </div>
+      <div className="cards_item">
+        <img src={img4} alt="img" />
+        <h3>FEATURES</h3>
+      </div>
+      <div className="cards_item">
+        <img src={img5} alt="img" />
+        <h3>DESIGN</h3>
+      </div>
+      <div className="cards_item">
+        <img src={img6} alt="img" />
+        <h3>SUPPORT</h3>
+      </div>
     </div>
   )
 }
--- src/components/text/Text.jsx
+++ src/components/text/Text.jsx
@@ -4,7 +4,23 @@ import './Text.css'
 function Text() {
   return (
     <div className='text'>
-
+      <div className="text_left">
+        <h1>The coolest workplaces <br /> offices in the world</h1>
+        <p>Best office design of group layout. Are coolest workplaces offices in the world. Architecture studio arquitectes life. Home office best design interior luxury offices. Office design ideas make work my home organization. Home office setup ideas offices in small furniture layout. Office interior design dreams house furniture cool interiors. Interior ideas decorating a home office of bamboo rod. Home office space ideas design small decorating. Bloc offices new york city office snapshots modern design. </p>
+        <button>read more</button>
+      </div>
+      <div className="text_right">
+        <div className="text_right_item1">
+          <img src="https://images01.nicepage.com/09/44/09445c0c818518d10fbacd13939d98bb.jpeg" alt="" />
+        </div>
+        <div className="text_right_item">
+          <div className="text_right_item_box">
+              <h1>10</h1>
+              <i>Office Ideas</i>
+          </div>
+          <i>Monday Consulting, an IT consultancy company based <br /> in Hamburg, Germany</i>
+        </div>
+      </div>
     </div>
   )
 }
